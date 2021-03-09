2c101fb65a199f4bd898c577b20a1829ad34615e
Simon Garton <simon.garton@gmail.com>
2021-03-10 06:47:32 +1300

    Improved TotalValueLine, 2 series

--- react-app/src/charts/TotalValueLine.js
+++ react-app/src/charts/TotalValueLine.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import {Scatter} from 'react-chartjs-2';
+import {
+  Scatter
+} from 'react-chartjs-2';
 import moment from 'moment';
 
 function formatDate(date) {
@@ -10,114 +12,159 @@ function formatDate(date) {
 
 class TotalValueColumn extends React.Component {
 
-    constructor(props) {
-        super();
-        this.state = {};
-    }
+  constructor(props) {
+    super();
+    this.state = {};
+    this.valueChartPoints = [];
+    this.spendingChartPoints = []
+  }
 
-    componentDidMount() {
-        fetch("https://g4spmx84mk.execute-api.ap-southeast-2.amazonaws.com/history")
-            .then(res => res.json())
-            .then(
-                (result) => {
-                this.setState({
-                    isLoaded: true,
-                    items: this.processData(result)
-                });
-                },
-                // Note: it's important to handle errors here
-                // instead of a catch() block so that we don't swallow
-                // exceptions from actual bugs in components.
-                (error) => {
-                this.setState({
-                    isLoaded: true,
-                    error
-                });
-                }
-            )
-        }
-
-    processData(data) {
-      let chartPoints = [];
-      data.forEach(element => {
-          let point = {
-            'x':moment(element['date']),
-            'y':element['value'],
-          }
-          chartPoints.push(point)
-      });
-      console.log(chartPoints)
-      let chartData = {
-        labels: ['Scatter'],
-        datasets: [
-          {
-            label: 'Total Value',
-            fill: false,
-            backgroundColor: 'rgba(75,192,192,0.4)',
-            pointBorderColor: 'rgba(75,192,192,1)',
-            pointBackgroundColor: '#fff',
-            pointBorderWidth: 5,
-            pointHoverRadius: 10,
-            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
-            pointHoverBorderColor: 'rgba(220,220,220,1)',
-            pointHoverBorderWidth: 2,
-            pointRadius: 1,
-            pointHitRadius: 10,
-            showLine: true,
-            data: chartPoints
-          }
-        ]
-      };
-      this.setState(chartData);
-    }
-
-    render() {
-        return <Scatter
-        data={this.state}
-        options={{
-          title:{
-            display:true,
-            text:'Current value',
-            fontSize:20
-          },
-          legend:{
-            display:false,
-            position:'right'
-          },
-          tooltips: {
-            mode: 'index',
-            intersect: false,
-            callbacks: {
-                label: function (t, d) {
-                  return formatDate(t.xLabel) + ' : $' + Math.round(t.value).toLocaleString();
-                }
-            }
+  componentDidMount() {
+    fetch("https://g4spmx84mk.execute-api.ap-southeast-2.amazonaws.com/history")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            valueData: this.processValue(result)
+          });
         },
-          scales: {
-            xAxes: [{
-                type: 'time',          
-                time: {
-                    unit: 'day'
-                },
-                ticks: {
-                  min:'2020-01-01',
-                  max:'2022-01-01'
-              }
-
-            }],
-            yAxes: [{
-              ticks: {
-                  callback: function(value, index, values) {
-                    //return value.toLocaleString("en-US",{style:"currency", currency:"USD"});
-                    return '$' + Math.round(value).toLocaleString();
-                  }
-              }
-          }]
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
         }
-        }}
-      />
+      )
+    fetch("https://g4spmx84mk.execute-api.ap-southeast-2.amazonaws.com/spending")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            spendingData: this.processSpending(result)
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
 
+  }
+
+  processValue(data) {
+    this.valueChartPoints = [];
+    data.forEach(element => {
+      let point = {
+        'x': moment(element['date']),
+        'y': element['value'],
+      }
+      this.valueChartPoints.push(point)
+    });
+    this.rebuildChart();
+  }
+
+  processSpending(data) {
+    this.spendingChartPoints = [];
+    let total = 0;
+    data.forEach(element => {
+      total = total + element['total'];
+      let point = {
+        'x': moment(element['date']),
+        'y': total,
+      }
+      this.spendingChartPoints.push(point)
+    });
+    this.rebuildChart();
+  }
+
+  rebuildChart() {
+    console.log(this.valueChartPoints);
+    console.log(this.spendingChartPoints);
+
+    let chartData = {
+      labels: ['Scatter'],
+      datasets: [
+        this.buildSeries('value', this.valueChartPoints, 'rgba(0,192,0,0.4)'),
+        this.buildSeries('spending', this.spendingChartPoints, 'rgba(192,0,0,0.4)')
+      ]
+    };
+    this.setState(chartData);
+  }
+
+  buildSeries(label, xyPoints, mainColor) {
+    return {
+      label: label,
+      fill: false,
+      borderColor: mainColor,
+      backgroundColor: mainColor,
+      pointBorderColor: 'rgba(0,0,0,1)',
+      pointBorderWidth: 1,
+      pointHoverRadius: 8,
+      pointHoverBorderColor: 'rgba(0,0,0,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: 4,
+      pointHitRadius: 10,
+      showLine: true,
+      lineTension: 0,
+      data: xyPoints
     }
   }
+
+  render() {
+    return <Scatter
+    data = {
+      this.state
+    }
+    options = {
+      {
+        title: {
+          display: true,
+          text: 'Spending vs Value',
+          fontSize: 20
+        },
+        legend: {
+          display: false,
+          position: 'right'
+        },
+        tooltips: {
+          mode: 'index',
+          intersect: false,
+          callbacks: {
+            label: function (t, d) {
+              return formatDate(t.xLabel) + ' : $' + Math.round(t.value).toLocaleString();
+            }
+          }
+        },
+        scales: {
+          xAxes: [{
+            type: 'time',
+            time: {
+              unit: 'day'
+            },
+            ticks: {
+              min: '2020-12-01',
+              max: '2021-07-01'
+            }
+
+          }],
+          yAxes: [{
+            ticks: {
+              callback: function (value, index, values) {
+                //return value.toLocaleString("en-US",{style:"currency", currency:"USD"});
+                return '$' + Math.round(value).toLocaleString();
+              }
+            }
+          }]
+        }
+      }
+    }
+    />
+
+  }
+}
 
 export default TotalValueColumn;
