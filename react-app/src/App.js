26c9118be3d9372e43e7a4da7f8023f78d34dae4
Simon Garton <simon.garton@gmail.com>
2021-03-19 11:17:02 +1300

    Gain and Loss chart

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TotalValueColumn from './charts/TotalValueColumn.js';
 import TotalValueLine from './charts/TotalValueLine.js';
+import GainLossColumn from './charts/GainLossColumn.js';
 
 // https://www.educative.io/edpresso/how-to-use-chartjs-to-create-charts-in-react
 
@@ -10,6 +11,7 @@ export default class App extends React.Component {
       <div>
         <TotalValueColumn/>
         <TotalValueLine/>
+        <GainLossColumn/>
       </div>
     );
   }
---
+++ react-app/src/charts/GainLossColumn.js
@@ -0,0 +1,236 @@
+import React from 'react';
+import {
+  Scatter
+} from 'react-chartjs-2';
+import moment from 'moment';
+
+function formatDate(date) {
+  const s = date.toLocaleString();
+  const parts = s.split(',')
+  return parts[0] + ',' + parts[1];
+}
+
+class GainLossColumn extends React.Component {
+
+  constructor(props) {
+    super();
+    this.state = {};
+    this.valueChartPoints = [];
+    this.spendingChartPoints = []
+    this.gainAndLossChartPoints = []
+  }
+
+  componentDidMount() {
+    fetch("https://g4spmx84mk.execute-api.ap-southeast-2.amazonaws.com/history")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            valueData: this.processValueFillInBlanks(result)
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+    fetch("https://g4spmx84mk.execute-api.ap-southeast-2.amazonaws.com/spending")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            spendingData: this.processSpendingFillInBlanks(result)
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  processValueFillInBlanks(data) {
+    this.valueChartPoints = [];
+    let actuals = {};
+    let minDate = null;
+    data.forEach(element => {
+      let thisDate = new Date(moment(element['date']));
+      console.log("thisDate ",thisDate," minDate ",minDate)
+      actuals[thisDate] = element['value']
+      if ((minDate == null) || (minDate > thisDate)) {
+        minDate = thisDate;
+      }
+    })  
+    console.log("got minDate " + minDate);
+    console.log("actuals");
+    console.log(actuals);
+    let total = 0;
+    for (var d = new Date(2021, 0, 1); d <= new Date(); d.setDate(d.getDate() + 1)) {
+      console.log(new Date(d));
+      let currentDate = d;
+      if (currentDate in actuals) {
+        total = actuals[currentDate]
+      }
+      let point = {
+        'x': moment(currentDate),
+        'y': total,
+      }
+      this.valueChartPoints.push(point)
+      console.log("pushing  ",point)
+      currentDate = currentDate + 1;
+    }
+    this.buildGainAndLoss();
+  }
+
+  processSpendingFillInBlanks(data) {
+    this.spendingChartPoints = [];
+    let actuals = {};
+    let minDate = null;
+    data.forEach(element => {
+      let thisDate = new Date(moment(element['date']));
+      console.log("thisDate ",thisDate," minDate ",minDate)
+      actuals[thisDate] = element['total']
+      if ((minDate == null) || (minDate > thisDate)) {
+        minDate = thisDate;
+      }
+    })  
+    console.log("got minDate " + minDate);
+    console.log("actuals");
+    console.log(actuals);
+    let total = 0;
+    for (var d = new Date(2021, 0, 1); d <= new Date(); d.setDate(d.getDate() + 1)) {
+      console.log(new Date(d));
+      let currentDate = d;
+      if (currentDate in actuals) {
+        total = total + actuals[currentDate]
+      }
+      let point = {
+        'x': moment(currentDate),
+        'y': total,
+      }
+      this.spendingChartPoints.push(point)
+      console.log("pushing  ",point)
+      currentDate = currentDate + 1;
+    }
+    this.buildGainAndLoss();
+  }
+
+  buildGainAndLoss() {
+    this.gainAndLossChartPoints = [];
+    if (this.spendingChartPoints.length !== this.valueChartPoints.length) {
+      this.rebuildChart();
+      return;  
+    }
+
+    if (this.spendingChartPoints.length === 0) {
+      this.rebuildChart();
+      return;  
+    }
+
+    for (let i = 0; i < this.spendingChartPoints.length; i++) {
+      let spending = this.spendingChartPoints[i];
+      let value = this.valueChartPoints[i];
+      this.gainAndLossChartPoints.push(
+        {
+          'x': value['x'],
+          'y': value['y'] - spending['y'],
+        }
+      )
+    }
+
+    this.rebuildChart();
+  }
+
+
+  rebuildChart() {
+    console.log(this.gainAndLossChartPoints);
+
+    let chartData = {
+      labels: ['Scatter'],
+      datasets: [
+        this.buildSeries('gain and loss', this.gainAndLossChartPoints, 'rgba(0,0,100,0.6)', false, null),
+      ]
+    };
+    this.setState(chartData);
+  }
+
+  buildSeries(label, xyPoints, mainColor, showPoints, pointStyle) {
+    return {
+      label: label,
+      fill: false,
+      borderColor: mainColor,
+      backgroundColor: mainColor,
+      pointBorderColor: 'rgba(0,0,0,1)',
+      pointBorderWidth: 1,
+      pointHoverRadius: 8,
+      pointHoverBorderColor: 'rgba(0,0,0,1)',
+      pointHoverBorderWidth: 2,
+      pointRadius: showPoints ? 4 : 0,
+      pointHitRadius: 10,
+      pointStyle,
+      showLine: true,
+      lineTension: 0,
+      data: xyPoints
+    }
+  }
+
+  render() {
+    return <Scatter
+    data = {
+      this.state
+    }
+    options = {
+      {
+        title: {
+          display: true,
+          text: 'Gain and Loss',
+          fontSize: 20
+        },
+        legend: {
+          display: false,
+          position: 'right'
+        },
+        tooltips: {
+          mode: 'index',
+          intersect: false,
+          callbacks: {
+            label: function (t, d) {
+              return formatDate(t.xLabel) + ' : $' + Math.round(t.value).toLocaleString();
+            }
+          }
+        },
+        scales: {
+          xAxes: [{
+            type: 'time',
+            time: {
+              unit: 'day'
+            },
+            ticks: {
+              min: '2020-12-01',
+              max: '2021-07-01'
+            }
+
+          }],
+          yAxes: [{
+            ticks: {
+              callback: function (value, index, values) {
+                //return value.toLocaleString("en-US",{style:"currency", currency:"USD"});
+                return '$' + Math.round(value).toLocaleString();
+              }
+            }
+          }]
+        }
+      }
+    }
+    />
+
+  }
+}
+
+export default GainLossColumn;
