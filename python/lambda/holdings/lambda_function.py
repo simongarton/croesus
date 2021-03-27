470c9c226f14ef9c11a87e9b69dafb270c2f7931
Simon Garton <simon.garton@gmail.com>
2021-03-27 18:56:58 +1300

    Holdings updated for account

--- python/lambda/holdings/lambda_function.py
+++ python/lambda/holdings/lambda_function.py
@@ -38,10 +38,7 @@ def lambda_handler(event, context):
             print("parsed query date as {}".format(query_date))
 
     if not "pathParameters" in event:
-        method = event["requestContext"]["http"]["method"]
-        if method == "GET":
-            return get_total_response(query_date)
-        return response(405, {"error": "unhandled method"})
+        return response(400, {"error": "no path parameters, need account"})
 
     method = event["requestContext"]["http"]["method"]
     parameters = event["pathParameters"]
@@ -56,39 +53,45 @@ def get(parameters, query_date):
         return get_symbol_response(parameters, query_date)
     if "exchange" in parameters:
         return get_exchange_response(parameters, query_date)
-    return get_total_response(query_date)
+    return get_total_response(parameters, query_date)
 
 
-def get_total_response(query_date):
-    return response(200, get_holdings(None, None, query_date))
+def get_total_response(parameters, query_date):
+    account = parameters["account"]
+    return response(200, get_holdings(account, None, None, query_date))
 
 
 def get_exchange_response(parameters, query_date):
     exchange = parameters["exchange"]
-    return response(200, get_holdings(exchange, None, query_date))
+    account = parameters["account"]
+    return response(200, get_holdings(account, exchange, None, query_date))
 
 
 def get_symbol_response(parameters, query_date):
     exchange = parameters["exchange"]
     symbol = parameters["symbol"]
-    return response(200, get_holdings(exchange, symbol, query_date))
+    account = parameters["account"]
+    return response(200, get_holdings(account, exchange, symbol, query_date))
 
 
-def get_holdings(exchange, symbol, query_date):
+def get_holdings(account, exchange, symbol, query_date):
     conn = get_database_connection()
     if not conn:
         return []
     print(conn)
     cur = conn.cursor()
-    cur.execute(
-        "SELECT id, date, exchange, symbol, quantity, price FROM transaction ORDER BY date, exchange, symbol"
-    )
+    sql = "SELECT id, date, exchange, symbol, account, quantity, price FROM transaction ORDER BY date, exchange, symbol"
+    params = []
+    if account != "all":
+        sql = "SELECT id, date, exchange, symbol, account, quantity, price FROM transaction WHERE account = %s ORDER BY date, exchange, symbol"
+        params = [account]
+    cur.execute(sql, params)
 
     transactions = cur.fetchall()
 
     holdings_map = {}
 
-    print("{} {} {}".format(exchange, symbol, query_date))
+    print("{} {} {} {}".format(exchange, symbol, account, query_date))
 
     for transaction in transactions:
         print(transaction)
@@ -105,18 +108,26 @@ def get_holdings(exchange, symbol, query_date):
             if transaction[3] != symbol:
                 print("failed symbol {} != {}".format(transaction[3], symbol))
                 continue
-        key = transaction[2] + ":" + transaction[3]
+        key = transaction[2] + ":" + transaction[3] + ":" + transaction[4]
         if not key in holdings_map:
             holdings_map[key] = 0
-        print("adding {}:{}".format(key, transaction[4]))
-        holdings_map[key] = holdings_map[key] + transaction[4]
+        print("adding {}:{}".format(key, transaction[5]))
+        holdings_map[key] = holdings_map[key] + transaction[5]
 
     holdings = []
     for key, value in holdings_map.items():
         parts = key.split(":")
         exchange = parts[0]
         symbol = parts[1]
-        holdings.append({"exchange": exchange, "symbol": symbol, "quantity": value})
+        account = parts[2]
+        holdings.append(
+            {
+                "exchange": exchange,
+                "symbol": symbol,
+                "account": account,
+                "quantity": value,
+            }
+        )
 
     # sort
     holdings.sort(key=lambda x: x["exchange"] + ":" + x["symbol"])
